a549252acbca7b275e7599e712e48f38a03430da
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2019-11-16 17:41:44 +0700

    added old2 files

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
@@ -10,7 +11,11 @@ http.createServer(function (req, res) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
     } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    if (path.extname(filename) == '.html') {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+    } else {
+      res.writeHead(200, {'Content-Type': 'image/png'});
+    }
     res.write(data);
     return res.end();
   });
